5fbcdc9a97c922c001f95ae9e35d6ce216de89ac
Stasec <vasypupkin1812@gmail.com>
2022-02-12 14:23:07 +0600

    создал новый проект

---
+++ MyCode.HelloWorld.CLI/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Почему я хочу стать разработчиком.");
+Console.WriteLine("Я уже долгое время работаю программистом поддержки, в среде Delphi.");
+Console.WriteLine("Хочется освоить язык с#, быть более востребованным на рынке труда, сменить работу.");
